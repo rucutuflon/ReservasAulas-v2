48064b4597c645712c0f0f7004d20139234ab181
rucut <rucut@DESKTOP-NBA2QKJ>
2019-03-10 12:20:45 +0100

    Commit de la clase PermanenciaPorHora

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/permanencia/PermanenciaPorHora.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/permanencia/PermanenciaPorHora.java
@@ -3,55 +3,85 @@ package org.iesalandalus.programacion.reservasaulas.modelo.dominio.permanencia;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class PermanenciaPorHora extends Permanencia{
 
-	private static final int PUNTOS = 0;
-	private static final int HORA_INICIO = 0;
-	private static final int HORA_FIN = 0;
-	private static final DateTimeFormatter FORMATO_HORA = DateTimeFormatter.ofPattern("HH:mm:ss");
+	private static final int PUNTOS = 3;
+	private static final int HORA_INICIO = 8;
+	private static final int HORA_FIN = 22;
+	private static final DateTimeFormatter FORMATO_HORA = DateTimeFormatter.ofPattern("HH:mm");
 	private LocalTime hora;
 	
 	public PermanenciaPorHora(LocalDate dia, LocalTime hora) {
-		this.dia = dia;
-		this.hora = hora;
+		super(dia);
+		setHora(hora);
 	}
 	
 	public PermanenciaPorHora(String dia, LocalTime hora) {
-		this.dia = dia;
-		this.hora = hora;
+		super(dia);
+		setHora(hora);
+	}
+	
+	public PermanenciaPorHora(LocalDate dia, String hora) {
+		super(dia);
+		setHora(hora);
 	}
 	
 	public PermanenciaPorHora(String dia, String hora) {
-		this.dia = dia;
-		this.hora = hora;
+		super(dia);
+		setHora(hora);
 	}
 	
 	public PermanenciaPorHora(PermanenciaPorHora p) {
-		this.dia = p.dia;
-        this.hora = p.hora;
+		if(p == null) {
+			throw new IllegalArgumentException("No se puede copiar una permanencia nula.");
+		}
+		setDia(p.dia);
+		setHora(p.hora);
 	}
+	
 
 	public LocalTime getHora() {
 		return hora;
 	}
 
 	private void setHora(LocalTime hora) {
+		if(hora == null) {
+			throw new IllegalArgumentException("La hora de una permanencia no puede ser nula.");
+		}
+		if(hora.getMinute() != 0) {
+			throw new IllegalArgumentException("La hora de una permanencia debe ser una hora en punto.");
+		}
+		if(hora.getHour() < HORA_INICIO || hora.getHour() > HORA_FIN) {
+			throw new IllegalArgumentException("La hora de una permanencia debe estar comprendida entre las 8 y las 22.");
+		}
 		this.hora = hora;
 	}
 	
-	private void setHora(String Tramo) {
-		this.tramo = tramo;
-	}
-
-	public static int getPuntos() {
-		return PUNTOS;
+	private void setHora(String hora) {
+		if(hora == null) {
+			throw new IllegalArgumentException("La hora de una permanencia no puede ser nula.");
+		}
+		try {
+			setHora(LocalTime.parse(hora, FORMATO_HORA));
+		} catch (DateTimeParseException e) {
+			throw new IllegalArgumentException("El formato de la hora de la permanencia no es correcto.");
+		}
 	}
 
 	@Override
+	public int getPuntos() {
+		return PUNTOS;
+	}
+
+	
+	
+	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = super.hashCode();
+		int result = 1;
+		result = prime * result + ((dia == null) ? 0 : dia.hashCode());
 		result = prime * result + ((hora == null) ? 0 : hora.hashCode());
 		return result;
 	}
@@ -60,11 +90,16 @@ public class PermanenciaPorHora extends Permanencia{
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (!super.equals(obj))
+		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		PermanenciaPorHora other = (PermanenciaPorHora) obj;
+		if (dia == null) {
+			if (other.dia != null)
+				return false;
+		} else if (!dia.equals(other.dia))
+			return false;
 		if (hora == null) {
 			if (other.hora != null)
 				return false;
@@ -75,8 +110,6 @@ public class PermanenciaPorHora extends Permanencia{
 
 	@Override
 	public String toString() {
-		return "PermanenciaPorHora [hora=" + hora + "]";
+		return "[dia="+FORMATO_DIA.format(dia)+", hora="+FORMATO_HORA.format(hora)+"]";
 	}
-	
-	
 }
