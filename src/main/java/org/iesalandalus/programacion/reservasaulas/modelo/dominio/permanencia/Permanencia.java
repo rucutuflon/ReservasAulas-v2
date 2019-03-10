a8b45fc2bdeee6dd03c9f50cda7a5b06d73b2f89
rucut <rucut@DESKTOP-NBA2QKJ>
2019-03-10 12:16:32 +0100

    Commit de la clase Permanencia

--- src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/permanencia/Permanencia.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/modelo/dominio/permanencia/Permanencia.java
@@ -1,83 +1,73 @@
 package org.iesalandalus.programacion.reservasaulas.modelo.dominio.permanencia;
 
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
+public abstract class Permanencia {
 
-public class Permanencia {
-        
-    private LocalDate dia;
-    private final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    private Tramo tramo;
-    
-    public Permanencia (LocalDate dia, Tramo tramo) {
-    	setDia(dia);
-        setTramo(tramo);
-    }
+	protected LocalDate dia;
+	protected static final DateTimeFormatter FORMATO_DIA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
-    public Permanencia(Permanencia p) {
-    	if(p == null) {
-    		throw new IllegalArgumentException("No se puede copiar una permanencia nula.");
-    	}
-    	this.dia = p.dia;
-        this.tramo = p.tramo;
-    }
+	protected Permanencia() {
+		this.dia = LocalDate.now();
+	}
+	
+	protected Permanencia(String s) {
+		setDia(s);
+	}
+	
+	protected Permanencia(LocalDate dia) {
+		setDia(dia);
+	}
+	
+//	public Permanencia(Permanencia p) {
+//		if(p == null) {
+//			throw new IllegalArgumentException("No se puede copiar una permanencia nula.");
+//		}
+//		this.dia = p.dia;
+//	}
 
 	public LocalDate getDia() {
 		return dia;
 	}
 
-	public void setDia(LocalDate dia) {
+	protected void setDia(LocalDate dia) {
 		if(dia == null) {
-    		throw new IllegalArgumentException("El dÃ­a de una permanencia no puede ser nulo.");
-    	}
+			throw new IllegalArgumentException("El día de una permanencia no puede ser nulo.");
+		}
 		this.dia = dia;
 	}
-
-	public Tramo getTramo() {
-		return tramo;
+	
+	protected void setDia(String s) {
+		if(s == null) {
+			throw new IllegalArgumentException("El día de una permanencia no puede ser nulo.");
+		}
+		try {
+			this.dia = LocalDate.parse(s, FORMATO_DIA);
+		} catch (DateTimeParseException e) {
+			throw new IllegalArgumentException("El formato del día de la permanencia no es correcto.");
+		}
+		
 	}
 
-	public void setTramo(Tramo tramo) {
-		if(tramo == null) {
-    		throw new IllegalArgumentException("El tramo de una permanencia no puede ser nulo.");
-    	}
-		this.tramo = tramo;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((dia == null) ? 0 : dia.hashCode());
-		result = prime * result + ((tramo == null) ? 0 : tramo.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Permanencia other = (Permanencia) obj;
-		if (dia == null) {
-			if (other.dia != null)
-				return false;
-		} else if (!dia.equals(other.dia))
-			return false;
-		if (tramo != other.tramo)
-			return false;
-		return true;
-	}
+//	public void setTramo(Tramo tramo) {
+//		if(tramo == null) {
+//			throw new IllegalArgumentException("El tramo de una permanencia no puede ser nulo.");
+//		}
+//		this.tramo = tramo;
+//	}
+	
+	public abstract int getPuntos();
 
 	@Override
-	public String toString() {
-		return "[dia=" + FORMATO_DIA.format(dia) + ", tramo=" + tramo + "]";
-	}
-    
-    
+	public abstract int hashCode();
+
+	@Override
+	public abstract boolean equals(Object obj);
+
+	@Override
+	public abstract String toString();
+	
+	
 }
